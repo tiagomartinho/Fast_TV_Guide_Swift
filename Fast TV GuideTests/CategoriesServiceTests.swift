c981b31a0b6d60019dc9452f420ac3c30a6daf51
Ronan O Ciosoig <ronan.o.ciosoig@stanwood.de>
2017-12-10 19:45:06 +0100

    Categories with failing test.

---
+++ Fast TV GuideTests/CategoriesServiceTests.swift
@@ -0,0 +1,30 @@
+//
+//  CategoriesServiceTests.swift
+//  Fast TV GuideTests
+//
+//  Created by Ronan on 10/12/2017.
+//  Copyright © 2017 Stanwood. All rights reserved.
+//
+
+import XCTest
+
+@testable import Fast_TV_Guide
+
+class CategoriesServiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testCategoriesService() {
+        let categories: [String] = CategoriesService.load()
+        XCTAssertNotNil(categories)
+        XCTAssertTrue(categories.count == 6)
+    }
+}
